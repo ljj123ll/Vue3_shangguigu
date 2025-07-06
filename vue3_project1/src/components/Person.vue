566ebcf88fdd97319ff55746a7a192d35a0cb615
ljj123ll <2915144166@qq.com>
2025-07-06 16:35:03 +0800

    refactor(src): 重构 App组件并添加 Person 组件
    - 移除了 HelloWorld 组件
    - 添加了 Person 组件
    - 更新了 App.vue 的模板和样式- 删除了 HelloWorld.vue 文件

---
+++ vue3_project1/src/components/Person.vue
@@ -0,0 +1,36 @@
+<templete>
+  <div class="person">
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+
+</templete>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  data() {
+    return {
+      name: '张三',
+      age: 18,
+      tel:'1388888888'
+    }
+  },
+  methods: {
+    showTel() {
+      alert(this.tel)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .person {
+    background-color: #646cff;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
